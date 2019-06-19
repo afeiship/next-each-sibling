114c40030a3dc39e8f0443eef9d0758468f8a688
afeiship <1290657123@qq.com>
2019-06-19 13:16:49 +0800

    add: intial & test

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-slice2arr
+> Slice array to two part
+
+## installation
+```bash
+npm install -S afeiship/next-slice2arr --registry=https://registry.npm.taobao.org
+```
+
+## usage
+```js
+//DOCS here!
+```
---
+++ src/next-slice2arr.js
@@ -0,0 +1,19 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+
+  nx.slice2arr = function(inArray, inIndex) {
+    var len = inArray.length;
+    if (len <= inIndex) {
+      nx.error('Index must less than array.length');
+    }
+    return [
+      inArray.slice(0, inIndex),
+      inArray.slice(inIndex),
+    ];
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.slice2arr;
+  }
+})();
